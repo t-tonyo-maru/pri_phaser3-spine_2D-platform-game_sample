8c1dc3b05cf8ff3e092195cdf25879a911fd5555
t-tonyo-maru <atsuji@atsujinoMacBook-Pro.local>
2024-01-04 19:13:04 +0900

    add: scene code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import './reset.css'
+import { Scene } from './scene'
+import { SpinePlugin } from '@esotericsoftware/spine-phaser'
 
 window.onload = () => {
-  console.log('hoge')
+  const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      parent: '',
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 1280,
+      height: 720
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+        debug: import.meta.env.PROD ? false : true
+      }
+    },
+    plugins: {
+      scene: [
+        { key: 'spine.SpinePlugin', plugin: SpinePlugin, mapping: 'spine' }
+      ]
+    },
+    scene: Scene
+  }
+  new Phaser.Game(config)
 }
